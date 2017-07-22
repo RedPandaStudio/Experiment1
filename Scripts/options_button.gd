1c2c652d07d11ab58bd1590fa0988364b8ac788a
maenstru56 <peteleaza.darius@yahoo.com>
2017-07-22 14:07:47 +0300

    Added Options Menu
    - Added options menu and sliders for sound and music volume

--- Scripts/options_button.gd
+++ Scripts/options_button.gd
@@ -1,10 +1,9 @@
 extends TextureButton
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	connect("pressed", self, "_on_pressed")
+	pass
+
+func _on_pressed():
+	get_tree().change_scene("res://Worlds/options_menu.tscn")
 	pass
